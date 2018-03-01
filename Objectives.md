804cbbfd4bdb50203e217bfa8cd6e104371faa21
Matthew Hyrka <mhyrka@gmail.com>
2018-03-01 11:54:33 -0700

    added objectives

---
+++ Objectives.md
@@ -0,0 +1,4 @@
+## OBJECTIVES
+
+There is treasure hidden in landmarks around the world! Follow clues to discover where!
+The team to reach the secret location in the fastest time wins!
--- places.js
+++ places.js
@@ -4,23 +4,33 @@ const places = [
       landmark: "Buckingham Palace",
       continent: {
         name: "Europe",
-        choices: [],
-        clue: ""
+        choices: ["North America", "South America", "Europe", "Asia", "Africa", "Australia", "Antarctica"],
+        clue: ["Home to the world's densest rail network",
+               "Home to the biggest island in the world",
+               "Makes 44% of the world's wine",
+               "Valetta is capital of country in this continent"]
       },
       subcontinent: {
         name: "United Kingdom",
-        choices: [],
-        clue: ""
+        choices: ["Southeast Pacific", "Eurasia", "Middle East", "United Kingdom"],
+        clue: ["Ruler is owner of 1/6th of the world's landmass", "Commonwealth of Bahamas gained independence from this collective in 1973",
+                "Home to a major monument older than the pyramids"]
       },
       country: {
         name: "England",
-        choices: [],
-        clue: ""
+        choices: ["Ireland", "Scotland", "Wales", "England"],
+        clue: ["has not had it's own separate government since 1707",
+               "cross of St. George is name of the flag",
+               "as of 2002 the population was ~49M",
+               "highest point is Scafell Pike"]
       },
       city: {
         name: "London",
-        choices: [],
-        clue: ""
+        choices: ["London", "Liverpool", "Leeds", "Birmingham"],
+        clue: ["previously called Ludenwic",
+               "6th most expensive city in the world",
+               "home to 20 subterranean rivers",
+               "over 300 languages are spoken here"]
       }
     }
   }
