c753be57c74c710563a821d69de16f11f07580c1
Matthew Hyrka <mhyrka@gmail.com>
2018-03-01 14:23:16 -0700

    almost there

--- game/app.js
+++ game/app.js
@@ -9,7 +9,7 @@ function displayClue() {
   let clueSection = document.querySelector('.clues');
 
   let clueDiv = document.createElement('div');
-
+  clueDiv.classList.add('clue');
   clueDiv.textContent = currentLandmark[rounds[currentRound]].clue[currentClue];
   clueSection.appendChild(clueDiv);
   currentClue++
@@ -25,14 +25,40 @@ function displayChoices() {
   for (var i = 0; i < currentLandmark[rounds[currentRound]].choices.length; i++) {
     let choiceDiv = document.createElement('div');
     choiceDiv.textContent = currentLandmark[rounds[currentRound]].choices[i];
+    choiceDiv.id = i;
+    choiceDiv.classList.add('choice');
     choices.appendChild(choiceDiv);
-    choiceDiv.addEventListener('click', guess)
+    addClickListener(choiceDiv);
+    // choiceDiv.addEventListener('click', guess)
+    // console.log(choiceDiv);
   }
 
 }
 
-function guess() {
-  
+function addClickListener(element){
+  element.addEventListener('click', function(){
+    guess(element)
+  })
+}
+
+function guess(element) {
+
+  if (element.textContent === currentLandmark[rounds[currentRound]].name) {
+    currentRound ++;
+    currentClue = 0;
+    Array.from(document.getElementsByClassName('choice')).forEach(c => {
+      document.querySelector('.choices').removeChild(c);
+    })
+    Array.from(document.getElementsByClassName('clue')).forEach(c => {
+      document.querySelector('.clues').removeChild(c);
+    })
+    displayChoices();
+  } else {
+    totalSeconds += 60;
+
+  }
+
+
 }
 
 displayChoices();
