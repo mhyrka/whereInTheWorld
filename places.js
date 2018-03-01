6db72e215497851ee895f56cf9aeaf586b9ab638
sean-blattenberger <sean.blattenberger@gmail.com>
2018-03-01 10:44:02 -0700

    Added data in places.js

---
+++ places.js
@@ -0,0 +1,27 @@
+const places = [
+  {
+    buckinghamPalace: {
+      landmark: "Buckingham Palace",
+      continent: {
+        name: "Europe",
+        choices: [],
+        clue: ""
+      },
+      subcontinent: {
+        name: "United Kingdom",
+        choices: [],
+        clue: ""
+      },
+      country: {
+        name: "England",
+        choices: [],
+        clue: ""
+      },
+      city: {
+        name: "London",
+        choices: [],
+        clue: ""
+      }
+    }
+  }
+];
